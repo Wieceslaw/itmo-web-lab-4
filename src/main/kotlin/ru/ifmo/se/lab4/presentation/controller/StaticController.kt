4386aee44773ebd6d020128143ef2d0e0a6260c3
wieceslaw <vyache-nov@mail.ru>
2023-01-13 21:53:43 +0300

    Error page fix

--- src/main/kotlin/ru/ifmo/se/lab4/presentation/controller/StaticController.kt
+++ src/main/kotlin/ru/ifmo/se/lab4/presentation/controller/StaticController.kt
@@ -1,12 +1,13 @@
 package ru.ifmo.se.lab4.presentation.controller
 
+import org.springframework.boot.web.servlet.error.ErrorController
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 
 
 @Controller
-class HtmlController {
-    @GetMapping("/", "/login")
+class StaticController: ErrorController {
+    @GetMapping("/", "/login", "/register", "/error")
     fun route(): String {
         return "index.html"
     }
